660f5b471d5322e3f42c03f8de6d205c0691995d
azagrasantos <carlosazagrasantos@gmail.com>
2022-08-27 16:00:14 +0200

    Create Sidebar structure and styles. Add component for buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css'
+import Sidebar from './components/Siderbar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
-        
+        <Sidebar/>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
