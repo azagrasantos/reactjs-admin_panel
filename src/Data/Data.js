9607481e786c54dc9229cadd4ec1fc67973fa9ae
azagrasantos <carlosazagrasantos@gmail.com>
2022-08-28 12:35:49 +0200

    Fix a RGB naming issue

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -69,7 +69,7 @@ export const CardsData = [
     {
         title: "Expenses",
         color: {
-            backGround: "linear-gradient(rgb(248, 212, 154) -146.42%, rgt(255 202 113) -46.42%)",
+            backGround: "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
             boxShadow: "0px 10px 20px 0px #F9D59B"
         },
         barValue: 60,
