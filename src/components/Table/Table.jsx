678f24582980fac6cd00bbecb5ff7b1ed354f630
azagrasantos <carlosazagrasantos@gmail.com>
2022-08-28 15:23:48 +0200

    Configure Card structure and css. Create static data to show a working scenario and create the base structure for Table

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Table.css'
+
+const Table = () => {
+  return (
+    <div className="Table">
+        <h3>Recent Orders</h3>
+    </div>
+  )
+}
+
+export default Table
